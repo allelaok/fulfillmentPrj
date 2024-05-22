3b544df3000989ddc2788ecec11726a39432b4bb
NaSoRim <lasolim@naver.com>
2024-05-22 15:02:20 +0900

    스웨거 삭제, 판매자 및 상품 수정 및 개발

--- fulfillment/src/main/java/com/springboot/fulfillment/service/SellerService.java
+++ fulfillment/src/main/java/com/springboot/fulfillment/service/SellerService.java
@@ -1,27 +1,35 @@
 package com.springboot.fulfillment.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.springboot.fulfillment.data.dto.SellerDTO;
 import com.springboot.fulfillment.data.entity.Seller;
 import com.springboot.fulfillment.data.repository.SellerRepository;
 
+@Service
 public class SellerService {
 
 	@Autowired
 	private SellerRepository sellerRepository;
 	
 	public Seller getSeller(String sellerId) {
-		return sellerRepository.findByIdContains(sellerId);
+		return sellerRepository.findBySellerIdContains(sellerId);
 	}
 	
 
 	public void addSeller(SellerDTO sellerDTO) {
 
-		Seller seller = Seller.builder().id(sellerDTO.getSellerId()).build();
+		Seller seller = Seller.builder()
+				.id(sellerDTO.getId())
+				.build();
 
 		sellerRepository.save(seller);
 	}
+	
+	public Seller getSellerIdByContact(SellerDTO sellerDTO) {
+		   return sellerRepository.findByContactContains(sellerDTO.getContact());
+		}
 	
 	
 //	public List<Seller> getSellerList() {
