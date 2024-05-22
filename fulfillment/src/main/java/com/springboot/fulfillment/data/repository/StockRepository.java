60d65b58cbfc330e936b3c0c977236da8ed157a2
jy <ji9626689@gmail.com>
2024-05-22 18:41:38 +0900

    stock 관련 다시 push

--- fulfillment/src/main/java/com/springboot/fulfillment/data/repository/StockRepository.java
+++ fulfillment/src/main/java/com/springboot/fulfillment/data/repository/StockRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.fulfillment.data.entity.Stock;
 
-public interface StockRepository extends JpaRepository<Stock, String>{
+public interface StockRepository extends JpaRepository<Stock, Long>{
 
 	List<Stock> findByStatusAndRegdate(int status, Date regdate);
 	long countByStatusAndRegdate(int status, Date regdate);
