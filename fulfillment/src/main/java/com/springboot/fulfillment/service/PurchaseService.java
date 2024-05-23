e2f7be7870cae46b69c1ff74aa16aad75f307a47
kcci <kcci@kcci109>
2024-05-23 22:05:38 +0900

    webclient메소드변경

--- fulfillment/src/main/java/com/springboot/fulfillment/service/PurchaseService.java
+++ fulfillment/src/main/java/com/springboot/fulfillment/service/PurchaseService.java
@@ -5,13 +5,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import com.springboot.fulfillment.data.dto.PurchaseRequestDTO;
-import com.springboot.fulfillment.data.dto.PurchaseResponseDTO;
 import com.springboot.fulfillment.data.entity.Goods;
 import com.springboot.fulfillment.data.entity.Purchase;
 import com.springboot.fulfillment.data.repository.GoodsRepository;
 import com.springboot.fulfillment.data.repository.PurchaseRepository;
+
+import reactor.core.publisher.Mono;
 
 @Service
 public class PurchaseService {
@@ -30,26 +32,62 @@ public class PurchaseService {
 
 	// Purchase 객체를 생성하여 제조사에게 전송하고 응답을 받은 후, 요청에 대한 DTO를 반환하는 메소드
 	public PurchaseRequestDTO sendPurchase(PurchaseRequestDTO purchase) {
-		System.out.println("===PurchaseController-updatePurchase===");
+		System.out.println("===PurchaseController-sendPurchase===");
 
-		// purchase객체를 responsedto로 변환
-		PurchaseResponseDTO purchaseResponseDTO = new PurchaseResponseDTO();
-		purchaseResponseDTO.setFkProductId(purchase.getFk_product_id());
-		purchaseResponseDTO.setOrderQuantity(purchase.getOrder_quantity());
-		purchaseResponseDTO.setOrderDate(purchase.getOrder_date());
-		purchaseResponseDTO.setOrderStatus(purchase.getOrder_status());
-		
+//		PurchaseResponseDTO purchaseResponseDTO = new PurchaseResponseDTO();
+//		purchaseResponseDTO.setFk_product_id(purchase.getFk_product_id());
+//		purchaseResponseDTO.setOrder_quantity(purchase.getOrder_quantity());
+//		purchaseResponseDTO.setOrder_date(purchase.getOrder_date());
+//		purchaseResponseDTO.setOrder_status(purchase.getOrder_status());
 		// webclient를 통해서 전송후에 반환되는 오더를 requestdto에 저장 후 리턴
-		PurchaseRequestDTO purchaseRequestDTO = webClient.post().uri("/order")// 서버로 보낼 uri
-				.bodyValue(purchaseResponseDTO).header("sender", "Purchase").retrieve()
-				.bodyToMono(PurchaseRequestDTO.class).block();
+	       try {
+	            PurchaseRequestDTO purchaseRequestDTO = webClient.post().uri("/order")
+	                .bodyValue(purchase)
+	                .header("sender", "Purchase")
+	                .retrieve()
+	                .onStatus(
+	                    status -> status.isError(),
+	                    clientResponse -> {
+	                        System.out.println("Error: " + clientResponse.statusCode());
+	                        return clientResponse.bodyToMono(String.class).flatMap(errorBody -> {
+	                            System.out.println("Error Body: " + errorBody);
+	                            return Mono.error(new RuntimeException("Failed to send purchase order: " + clientResponse.statusCode()));
+	                        });
+	                    }
+	                )
+	                .bodyToMono(PurchaseRequestDTO.class)
+	                .block();
 
-		return purchaseRequestDTO;
+	            return purchaseRequestDTO;
+	        } catch (WebClientResponseException e) {
+	            System.out.println("WebClientResponseException: " + e.getMessage());
+	            // handle WebClientResponseException
+	        } catch (Exception e) {
+	            System.out.println("Exception: " + e.getMessage());
+	            // handle other exceptions
+	        }
+		return purchase;
+		
+		
+//		PurchaseRequestDTO purchaseRequestDTO = webClient.post().uri("/order")
+//				.bodyValue(purchase).header("sender", "Purchase")
+//				.exchangeToMono(response -> {
+//					if(response.statusCode().is2xxSuccessful()) {
+//						return response.bodyToMono(PurchaseRequestDTO.class);
+//					}else {
+//						System.out.println("errrrrrrrrrrrrrrrrrr: " + response.createException().flatMap(Mono::error));
+//						return response.bodyToMono(PurchaseRequestDTO.class);
+//					}
+//				})
+//				.block();
+		
+
+//		return purchaseRequestDTO;
 	}
 
 	// Purchase 객체를 저장하는 메소드
 	public void addPurchase(PurchaseRequestDTO purchaseRequestDTO) {
-		System.out.println("===PurchaseController-updatePurchase===");
+		System.out.println("===PurchaseController-addPurchase===");
 
 		// PurchaseRequestDTO를 Purchase 엔티티로 변환하여 저장하는 로직을 구현
 		Purchase purchase = new Purchase();
@@ -71,11 +109,28 @@ public class PurchaseService {
 	}
 
 	// Purchase 객체를 업데이트하는 메소드
-	public void updatePurchase(Purchase updatedPurchase) {
+	public void updatePurchase(PurchaseRequestDTO updatedPurchase) {
 		System.out.println("===PurchaseController-updatePurchase===");
 
+		Optional<Goods> goods = goodsRepository.findById(updatedPurchase.getFk_product_id());
+		
+		
+		Purchase purchase = new Purchase();
+		purchase.setPurchaseId(updatedPurchase.getOrder_id().toString());
+		if (goods.isPresent()) {
+			purchase.setGoods(goods.get());
+		} else {
+			throw new RuntimeException();
+		}
+		purchase.setPurchaseDate(updatedPurchase.getOrder_date());
+		purchase.setPurchaseStatus(updatedPurchase.getOrder_status());
+		purchase.setQuantity(updatedPurchase.getOrder_quantity());
+		
+		
+		
+		
 		// 업데이트할 Purchase 객체를 레퍼지토리의 save 메소드를 통해 저장하는 로직을 구현
-		purchaseRepository.save(updatedPurchase);
+		purchaseRepository.save(purchase);
 	}
 
 }
