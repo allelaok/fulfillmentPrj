538df45ae49c2e53a0e3600cc8ce32f87782401a
kcci <kcci@kcci109>
2024-05-24 10:24:55 +0900

    누락된 사항 추가 업데이트 - 문승환

--- fulfillment/src/main/java/com/springboot/fulfillment/controller/PurchaseController.java
+++ fulfillment/src/main/java/com/springboot/fulfillment/controller/PurchaseController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,23 +26,23 @@ public class PurchaseController {
 
 	// body를 통해서 들어온 purchase객체를 통해 서비스에 전송하여 응답받은 id가 담겨있는 responseDTO를 통해 풀필먼트테이블에 저장
 	@PostMapping
-	public void addPurchase(@RequestBody PurchaseRequestDTO purchase) {
+	public void addPurchase(@RequestBody PurchaseRequestDTO purchaseRequestDTO) {
 		System.out.println("===PurchaseController-addPurchase===");
 		
 		// 서비스로 객체를 보내어 dto로 변경하여 서버로전송(제조사)
-		PurchaseRequestDTO purchaseRequestDTO = purchaseService.sendPurchase(purchase);
+		PurchaseRequestDTO rePurchaseRequestDTO = purchaseService.sendPurchase(purchaseRequestDTO);
 
 		// 응답받은 requestDTO를 통해서 풀필먼트테이블에 저장
-		purchaseService.addPurchase(purchaseRequestDTO);
+		purchaseService.addPurchase(rePurchaseRequestDTO);
 	}
 
 	// 전달받은 업데이트발주요청(상태변화)를 서비스에 전달하여 저장
 	@PutMapping
-	public ResponseEntity<String> updatePurchase(@RequestBody Purchase updatedPurchase) {
+	public ResponseEntity<String> updatePurchase(@RequestBody PurchaseRequestDTO purchaseRequestDTO, @RequestHeader("sender") String sender) {
 		System.out.println("===PurchaseController-updatePurchase===");
 		
 		// 전달받은 업데이트발주요청(상태변화)를 서비스에 전달하여 저장
-		purchaseService.updatePurchase(updatedPurchase);
+		purchaseService.updatePurchase(purchaseRequestDTO);
 
 		return ResponseEntity.ok("Purchase updated successfully");
 	}
--- fulfillment/src/main/java/com/springboot/fulfillment/data/repository/PurchaseRepository.java
+++ fulfillment/src/main/java/com/springboot/fulfillment/data/repository/PurchaseRepository.java
@@ -1,5 +1,7 @@
 package com.springboot.fulfillment.data.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.fulfillment.data.entity.Goods;
@@ -7,4 +9,5 @@ import com.springboot.fulfillment.data.entity.Purchase;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long>{
 	Purchase findByNo(Goods goodsId);
+	Optional<Purchase> findByPurchaseId(String purchaseId);
 }
